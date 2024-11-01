e03abe22132cdb3cf99a12c457449a581eb7612c
alejo8903 <alejo890327@gmail.com>
2024-11-01 02:02:18 +0100

    se corrige controller y se agrega vista principal

--- src/main/java/com/iud/ea1/controller/FuncionarioController.java
+++ src/main/java/com/iud/ea1/controller/FuncionarioController.java
@@ -2,42 +2,44 @@ package com.iud.ea1.controller;
 
 import com.iud.ea1.dao.FuncionarioDAO;
 import com.iud.ea1.domain.Funcionario;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 
-@RestController
-@RequestMapping("/funcionarios")
+
+
 
 public class FuncionarioController {
 
-    @Autowired
-    private FuncionarioDAO funcionarioDAO;
 
-    @PostMapping
-    public void crearFuncionario(@RequestBody Funcionario funcionario) {
+    private FuncionarioDAO funcionarioDAO;
+    
+    public FuncionarioController(){
+        funcionarioDAO = new FuncionarioDAO();
+    }
+
+
+    public void crearFuncionario(Funcionario funcionario) {
     funcionarioDAO.crearFuncionario(funcionario);
     }
 
-    @GetMapping
+
     public List<Funcionario> obtenerFuncionarios() {
     return funcionarioDAO.obtenerFuncionarios();
     }
 
-    @GetMapping("/{id}")
-    public Funcionario obtenerFuncionarioPorId(@PathVariable int id) {
+
+    public Funcionario obtenerFuncionarioPorId(int id) {
     return funcionarioDAO.obtenerPorId(id);
     }
 
-    @PutMapping("/{id}")
-    public void editarFuncionarioPorId(@PathVariable int id, @RequestBody Funcionario nuevoFuncionario) {
+
+    public void editarFuncionarioPorId(int id,Funcionario nuevoFuncionario) {
     funcionarioDAO.editarFuncionarioPorId(id, nuevoFuncionario);
     }
 
-    @DeleteMapping("/{id}")
-    public void eliminarFuncionarioPorId(@PathVariable int id) {
+
+    public void eliminarFuncionarioPorId(int id) {
     funcionarioDAO.eliminarFuncionarioPorId(id);
     }
 }
--- src/main/java/com/iud/ea1/dao/GrupoFamiliarDAO.java
+++ src/main/java/com/iud/ea1/dao/GrupoFamiliarDAO.java
@@ -59,7 +59,6 @@ public class GrupoFamiliarDAO {
     em.getTransaction().commit();
     em.close();
     }
-    }
-
 }
+
 
